a1d80ca0635706627729dcb7030ce12e2316bc4d
sumitpanwar10 <sumitpanwar.professional@gmail.com>
2022-05-26 16:08:49 +0530

    Updates

---
+++ precache-manifest.00fbc8674e8a25213dd3c0ad0104ac7d.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b47a9c7be78cf34043f39a2fd340e91",
+    "url": "/To-Do-List/index.html"
+  },
+  {
+    "revision": "9302e28affcbd310209f",
+    "url": "/To-Do-List/static/js/2.17e1ab02.chunk.js"
+  },
+  {
+    "revision": "d0b9f43c7ae3c511e6ec",
+    "url": "/To-Do-List/static/js/main.eb6f0ea0.chunk.js"
+  },
+  {
+    "revision": "4936381a0711d56acf0f",
+    "url": "/To-Do-List/static/js/runtime-main.65276c1b.js"
+  }
+]);
